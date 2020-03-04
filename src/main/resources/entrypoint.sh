95dbdf264c6e7be025e2887ff80e51227b731a1f
MaNa <malkoas@ya.ru>
2020-03-04 14:01:42 +0300

    remove bin-data, dockerfile, entrypoint.sh

--- src/main/resources/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-function onExit {
-    pg_ctl -D /var/lib/postgresql/@postgresql.version@/data stop -w
-}
-trap onExit EXIT
-
-pg_ctl -D /var/lib/postgresql/@postgresql.version@/data start -w
-$@
-exit $?
