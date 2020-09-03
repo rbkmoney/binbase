3364943ae17dcfd8a727d32f9f3a687da04018c2
malkoas <41993717+malkoas@users.noreply.github.com>
2020-09-03 15:31:06 +0300

    change BinBaseData

--- src/main/java/com/rbkmoney/binbase/batch/BinBaseCsvData.java
+++ src/main/java/com/rbkmoney/binbase/batch/BinBaseCsvData.java
@@ -8,17 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class BinBaseCsvData extends BinBaseData {
 
-    private String bin;
-    private String brand;
-    private String bank;
-    private String type;
-    private String category;
-    private String isoname;
-    private String isoa2;
-    private String isoa3;
-    private String isonumber;
-    private String url;
-    private String phone;
     private String binLength;
     private String affiliation;
     private String mark;
